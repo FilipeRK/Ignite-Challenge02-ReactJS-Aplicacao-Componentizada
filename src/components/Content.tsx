df34a69257d9c447631cf0698f63c7a2c80eca2d
Filipe Kohls <filiperenank@gmail.com>
2021-03-15 22:22:47 -0300

    Componentização concluída 🚀

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,49 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from 'react'
+import { api } from '../services/api'
+import { Header } from './Header'
+import { MovieCard } from './MovieCard'
+
+interface MovieProps {
+  imdbID: string
+  Title: string
+  Poster: string
+  Ratings: Array<{
+    Source: string
+    Value: string
+  }>
+  Runtime: string
+}
+
+interface ContentProps {
+  genreId: number
+  genreTitle: string
+}
+
+export function Content({ genreId, genreTitle } : ContentProps) {
+  const [movies, setMovies] = useState<MovieProps[]>([])
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${genreId}`).then(response => {
+      setMovies(response.data)
+    })
+  }, [genreId])
+
+  return (   
+    <div className="container">
+      <Header title={genreTitle} />
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
